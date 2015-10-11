1791ce188b19a966b59c822ad3ddf6cee3346ac8
Kyle <KyleMit@gmail.com>
2015-10-11 14:42:13 -0400

    added template for editing

--- client/templates/exercise.js
+++ client/templates/exercise.js
@@ -2,28 +2,37 @@ Template.exercise.helpers({
   "exercise": () => {
     // path: /exercise/edit/:id
     let id = FlowRouter.getParam('id');
-    console.log(id);
+
     return Exercises.findOne(id);
   }
 });
 
 Template.exercise.events({
-  "submit form": (event) => {
+  "submit form": (event, template) => {
     event.preventDefault();
+
 
     // get values from form
     let n = event.target.inputName.value;
     let d = event.target.inputDescription.value;
     let t = event.target.inputType.value;
 
-    // insert into db
-    let exerciseId = Exercises.insert({
+    let e = {
       name: n,
       description: d,
       type: t
-    });
+    }
 
-    FlowRouter.go('/exercises/' + exerciseId);
+    // grab id (if we have one)
+    let id = FlowRouter.getParam('id')
+
+    // insert into db
+    let result = Exercises.upsert(id, e);
+
+    // get resulting ID or id from route
+    let resultID = result.insertedId || id
+
+    FlowRouter.go('/exercises/' + resultID);
   }
 });
 
