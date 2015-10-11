da11271983373259ffbbf47ffc7383d003c0969a
Kyle <KyleMit@gmail.com>
2015-10-11 12:40:29 -0400

    Add initialize function on template rendered

--- client/templates/addExcercise.js
+++ client/templates/addExcercise.js
@@ -2,18 +2,27 @@ Template.addExercise.events({
   "submit form": function (event) {
     event.preventDefault();
 
+    // get values from form
     let n = event.target.inputName.value;
     let d = event.target.inputDescription;
     let t = event.target.inputType;
 
+    // insert into db
     Exercises.insert({
       name: n,
       description: d,
       type: t
     });
 
+    // clear form
     event.target.inputName.value = "";
     event.target.inputDescription = "";
     event.target.inputType.checked = false;
   }
+});
+
+Template.addExercise.onRendered(
+  () => {
+    // initialize material library
+    $.material.init()
 });
